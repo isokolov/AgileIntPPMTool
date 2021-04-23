6a599e8c72d55b9256f5bb9029868ecc426d679a
Illya Sokolov <illya.sokolov82@gmail.com>
2021-04-23 11:30:00 +0300

    project item and header

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -1,10 +1,12 @@
 // import logo from './logo.svg';
 import './App.css';
 import Dashboard from './components/Dashboard';
+import Header from './components/Layout/Header';
 
 function App() {
   return (
     <div className="App">
+    <Header />
     <Dashboard />
       {/*<header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
--- ppmtool-react-client/src/components/Dashboard.js
+++ ppmtool-react-client/src/components/Dashboard.js
@@ -9,12 +9,15 @@
 */
 
 import React, { Component } from 'react'
+import ProjectItem from './Project/ProjectItem'
 
 class Dashboard extends Component {
     render() {
         return (
             <div>
                 <h1>Welcome to the Dashboard!</h1>
+                <ProjectItem />
+                <ProjectItem />
             </div>
         )
     }
