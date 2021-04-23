6187a7a848ff68638ea939ed5ddbfaac3f95ccbf
Illya Sokolov <illya.sokolov82@gmail.com>
2021-04-23 12:02:08 +0300

    Adding Styles to Components from Templates

--- ppmtool-react-client/src/components/Dashboard.js
+++ ppmtool-react-client/src/components/Dashboard.js
@@ -14,13 +14,21 @@ import ProjectItem from './Project/ProjectItem'
 class Dashboard extends Component {
     render() {
         return (
-            <div>
-            {/* In your project folder run npm install bootstrap
-             to add bootstrap to your project
-             test to check if bootstrap works */}
-                <h1 className="alert alert-warning">Welcome to the Dashboard!</h1>
-                <ProjectItem />
-                <ProjectItem />
+            <div className="projects">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              <h1 className="display-4 text-center">Projects</h1>
+              <br />
+              <a href="ProjectForm.html" className="btn btn-lg btn-info">
+                Create a Project
+              </a>
+              <br />
+              <hr />
+              <ProjectItem />
+            </div>
+          </div>
+        </div>
             </div>
         )
     }
