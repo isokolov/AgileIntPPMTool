a0a9bcb586833c563e7d3bd09a31027a7b098a13
Illya Sokolov <illya.sokolov82@gmail.com>
2021-04-25 16:12:34 +0300

    get project backlog

--- PPMToolFullStack/src/main/java/io/agileintelligence/ppmtool/domain/Project.java
+++ PPMToolFullStack/src/main/java/io/agileintelligence/ppmtool/domain/Project.java
@@ -1,6 +1,7 @@
 package io.agileintelligence.ppmtool.domain;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import org.springframework.validation.annotation.Validated;
 
 import javax.persistence.*;
@@ -34,6 +35,7 @@ public class Project {
     private Date updated_At;
 
     @OneToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL, mappedBy = "project")
+    @JsonIgnore
     private Backlog backlog;
 
     public Backlog getBacklog() {
--- PPMToolFullStack/src/main/java/io/agileintelligence/ppmtool/web/BacklogController.java
+++ PPMToolFullStack/src/main/java/io/agileintelligence/ppmtool/web/BacklogController.java
@@ -27,12 +27,22 @@ public class BacklogController {
     public ResponseEntity<?> addPTtoBacklog(@Valid @RequestBody ProjectTask projectTask,
                                             BindingResult result, @PathVariable String backlog_id){
 
+        //show delete
+        //custom exception
+
         ResponseEntity<?> erroMap = mapValidationErrorService.MapValidationService(result);
         if (erroMap != null) return erroMap;
 
         ProjectTask projectTask1 = projectTaskService.addProjectTask(backlog_id, projectTask);
 
         return new ResponseEntity<ProjectTask>(projectTask1, HttpStatus.CREATED);
+
+    }
+
+    @GetMapping("/{backlog_id}")
+    public Iterable<ProjectTask> getProjectBacklog(@PathVariable String backlog_id){
+
+        return projectTaskService.findBacklogById(backlog_id);
 
     }
 
