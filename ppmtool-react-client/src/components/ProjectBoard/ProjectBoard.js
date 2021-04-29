eabdf270dc161e47d8c52bf3375fef19c6c7c875
Illya Sokolov <illya.sokolov82@gmail.com>
2021-04-29 14:36:04 +0300

    display tasks step1

--- ppmtool-react-client/src/components/ProjectBoard/ProjectBoard.js
+++ ppmtool-react-client/src/components/ProjectBoard/ProjectBoard.js
@@ -6,14 +6,16 @@ import PropTypes from "prop-types";
 import { getBacklog } from "../../actions/backlogActions";
 
 class ProjectBoard extends Component {
+  //constructor to handle errors
 
-    componentDidMount() {
-        const { id } = this.props.match.params;
-        this.props.getBacklog(id);
-      }
-    
+  componentDidMount() {
+    const { id } = this.props.match.params;
+    this.props.getBacklog(id);
+  }
+
   render() {
     const { id } = this.props.match.params;
+    const { project_tasks } = this.props.backlog;
     return (
       <div className="container">
         <Link to={`/addProjectTask/${id}`} className="btn btn-primary mb-3">
@@ -21,25 +23,22 @@ class ProjectBoard extends Component {
         </Link>
         <br />
         <hr />
-        {
-          // <!-- Backlog STARTS HERE -->
-        }
-        <Backlog />
+        <Backlog project_tasks_prop={project_tasks} />
       </div>
     );
   }
 }
 
 ProjectBoard.propTypes = {
-    backlog: PropTypes.object.isRequired,
-    getBacklog: PropTypes.func.isRequired
-  };
-  
-  const mapStateToProps = state => ({
-    backlog: state.backlog
-  });
-  
-  export default connect(
-    mapStateToProps,
-    { getBacklog }
-  )(ProjectBoard);
+  backlog: PropTypes.object.isRequired,
+  getBacklog: PropTypes.func.isRequired
+};
+
+const mapStateToProps = state => ({
+  backlog: state.backlog
+});
+
+export default connect(
+  mapStateToProps,
+  { getBacklog }
+)(ProjectBoard);
