8d2ee2c6413725117f810523b3803a1982a8ec70
Illya Sokolov <illya.sokolov82@gmail.com>
2021-05-09 17:31:55 +0300

    handle login action

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -1,20 +1,39 @@
-// import logo from './logo.svg';
-import './App.css';
-import Dashboard from './components/Dashboard';
-import Header from './components/Layout/Header';
+import React, { Component } from "react";
+import "./App.css";
+import Dashboard from "./components/Dashboard";
+import Header from "./components/Layout/Header";
 import "bootstrap/dist/css/bootstrap.min.css";
-import {BrowserRouter as Router, Route} from "react-router-dom"
-import AddProject from './components/Project/AddProject';
-import { Component } from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import AddProject from "./components/Project/AddProject";
 import { Provider } from "react-redux";
 import store from "./store";
-import UpdateProject from './components/Project/UpdateProject';
+import UpdateProject from "./components/Project/UpdateProject";
 import ProjectBoard from "./components/ProjectBoard/ProjectBoard";
 import AddProjectTask from "./components/ProjectBoard/ProjectTasks/AddProjectTask";
 import UpdateProjectTask from "./components/ProjectBoard/ProjectTasks/UpdateProjectTask";
 import Landing from "./components/Layout/Landing";
 import Register from "./components/UserManagement/Register";
 import Login from "./components/UserManagement/Login";
+import jwt_decode from "jwt-decode";
+import setJWTToken from "./securityUtils/setJWTToken";
+import { SET_CURRENT_USER } from "./actions/types";
+
+const jwtToken = localStorage.jwtToken;
+
+if (jwtToken) {
+  setJWTToken(jwtToken);
+  const decoded_jwtToken = jwt_decode(jwtToken);
+  store.dispatch({
+    type: SET_CURRENT_USER,
+    payload: decoded_jwtToken
+  });
+
+  const currentTime = Date.now() / 1000;
+  if (decoded_jwtToken.exp < currentTime) {
+    //handle logout
+    //window.location.href = "/";
+  }
+}
 
 class App extends Component {
   render() {
